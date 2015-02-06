732e19a68b57f8155aef2270a1048ef32f1e0c7a
Miguel Ángel Fernández Sánchez <mafesan.nsn@gmail.com>
2015-02-06 10:00:23 +0100

    Initial commit
    Creación de archivos, gitignore y primer programa (Saluton.java)

---
+++ src/Saluton.java
@@ -0,0 +1,7 @@
+public class Saluton{
+    public static void main(String[] arguments){
+        //Saluda al mundo en esperanto
+        String greeting = "Saluton mondo!";
+        System.out.println(greeting);
+    }
+}
