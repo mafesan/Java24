dc2f416bb06f6b172b56f1f990e01acc9ca3dbbd
Miguel Ángel Fernández <mafesan.nsn@gmail.com>
2015-02-07 17:47:17 +0100

    Ejercicio 'Root' - Tema 4 Curso Java
    Creación de un programa que calcula la raíz cuadrada de un numero e
    imprime por pantalla el resultado

---
+++ src/Root.java
@@ -0,0 +1,15 @@
+
+//Programa que calcula la 
+//raiz cuadrada de un numero e imprime
+//por pantalla el resultado
+
+class Root{
+    public static void main(String[] arguments){
+        int number = 225;
+        System.out.println("The square root of "
+            + number
+            + " is "
+            + Math.sqrt(number)
+        );
+    }
+}
