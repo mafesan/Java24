410026f9302e6c3873011d754121937209b4682c
Miguel Angel Fernandez <mafesan.nsn@gmail.com>
2015-02-15 14:01:36 +0100

    Programa Credits del tema 6 (Strings)

---
+++ src/Credits.java
@@ -0,0 +1,40 @@
+class Credits {
+
+    public static void main(String[] args) {
+        //Definir información sobre la película
+        String name = "credits";
+        String title = "Interstellar";
+        int year = 2014;
+        String director = "Christopher Nolan";
+        String role1 = "Cooper";
+        String actor1 = "Matthew McGonnahey";
+        String role2 = "Brand";
+        String actor2 = "Anne Hathaway";
+        String role3 = "Murph";
+        String actor3 = "Jessica Chastain";
+        String role4 = "Professor Brand";
+        String actor4 = "Michael Caine";
+        //Mostrar información
+        String newName = name.toUpperCase();
+        //Otra opcion: string.toLowerCase();
+        
+        System.out.println(newName);
+        System.out.println(title + " (" + year + ") \n" +
+            "A " + director + " film. \n\n"
+            + role1 + "\t" + actor1 + "\n"
+            + role2 + "\t" + actor2 + "\n"
+            + role3 + "\t" + actor3 + "\n"
+            + role4 + "\t" + actor4);
+        
+        System.out.println("\n\nThe name " + title + " has " +
+            title.length() + " letters");
+        
+        char searchChar = 'w';
+        System.out.println("\nThe name " + actor1 + " has the letter "
+            + searchChar + " in position " + actor1.indexOf(searchChar));
+        
+        String guess = "Tom Cruise";
+        System.out.println("\nIs " + role1 + " role played by " + guess + "?");
+        System.out.println("Answer: " + actor1.equals(guess));
+    }
+}
