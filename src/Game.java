18ca280184d4fede907c62895940fb92b5627a6d
Miguel Ángel Fernández <mafesan.nsn@gmail.com>
2015-02-19 23:30:48 +0100

    Tema Ifs + Switch

--- src/Game.java
+++ src/Game.java
@@ -20,7 +20,7 @@ class Game {
             System.out.println("Extra Life!");
             difficultyLevel += 5;
             System.out.println("New Difficulty: " + difficultyLevel);
-            
+        }   
         /*
             Notas:
             Se pueden usar los operadores == y != con todos los tipos
@@ -31,6 +31,46 @@ class Game {
             if (account <= 0) System.out.println("No more money");
             
         */
+            
+         int answer = 17;
+         int correctAnswer = 13;
+         
+         if (answer == correctAnswer) {
+             score += 10;
+             System.out.println("That's right. You get 10 points");
+         } else {
+             score -= 5;
+             System.out.println("Sorry, that's wrong. You lose 5 points");
+         }
+         
+        char grade = 'B';
+         
+        if (grade == 'A') {
+            System.out.println("You got an A. Great job!");
+        } else if (grade == 'B') {
+            System.out.println("You got a B. Good work!");
+        } else if (grade == 'C') {
+            System.out.println("You got a C. What went wrong?");
+        } else {
+            System.out.println("You got a F. You'll do well in Congress!");
         }
+        
+         
+         //Otra forma: Switch
+         
+        char grade2 = 'A';
+        switch (grade2) {
+            case 'A':
+                System.out.println("You got an A. Great job!");
+                break;
+            case 'B':
+                System.out.println("You got a B. Good work!");
+                break;
+            case 'C':
+                System.out.println("You got a C. What went wrong?");
+                break;
+            default:
+                System.out.println("You got a F. You'll do well in Congress!");
+         }
     }
 }
