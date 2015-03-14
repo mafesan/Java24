29796a792bb08944176c6986913cd3a80e3a2f5c
Miguel Angel Fernandez <mafesan.nsn@gmail.com>
2015-03-14 21:23:54 +0100

    minor changes

--- src/SimpleOperations.java
+++ src/SimpleOperations.java
@@ -2,7 +2,13 @@ class SimpleOperations {
     public static void main(String args[]) {
         int x = 5;
         int y = 3;
+        String palabra;
         double answer = Math.pow(x, 2) + Math.pow(y, 2);
         System.out.println(answer);
+        palabra = "Hola";
+        System.out.println(palabra);
+        palabra = "Adios";
+        System.out.println(palabra);
+        
     }
 }
